f179dd1d26f3acbb1a0ff79a695a828dcb312213
evondev <trananhtuan400@gmail.com>
2021-06-24 22:44:44 +0700

    Complete simple-slider ex

---
+++ exercises/simple-slider/final.js
@@ -0,0 +1,48 @@
+window.addEventListener("load", function () {
+  const sliderMain = document.querySelector(".slider-main");
+  const sliderItems = document.querySelectorAll(".slider-item");
+  const nextBtn = document.querySelector(".slider-next");
+  const prevBtn = document.querySelector(".slider-prev");
+  const dotItems = document.querySelectorAll(".slider-dot-item");
+  const sliderItemWidth = sliderItems[0].offsetWidth;
+  const slidesLength = sliderItems.length;
+  let postionX = 0;
+  let index = 0;
+  nextBtn.addEventListener("click", function () {
+    handleChangeSlide(1);
+  });
+  prevBtn.addEventListener("click", function () {
+    handleChangeSlide(-1);
+  });
+  [...dotItems].forEach((item) =>
+    item.addEventListener("click", function (e) {
+      [...dotItems].forEach((el) => el.classList.remove("active"));
+      e.target.classList.add("active");
+      const slideIndex = parseInt(e.target.dataset.index);
+      index = slideIndex;
+      postionX = -1 * index * sliderItemWidth;
+      sliderMain.style = `transform: translateX(${postionX}px)`;
+    })
+  );
+  function handleChangeSlide(direction) {
+    if (direction === 1) {
+      if (index >= slidesLength - 1) {
+        index = slidesLength - 1;
+        return;
+      }
+      postionX = postionX - sliderItemWidth;
+      sliderMain.style = `transform: translateX(${postionX}px)`;
+      index++;
+    } else if (direction === -1) {
+      if (index <= 0) {
+        index = 0;
+        return;
+      }
+      postionX = postionX + sliderItemWidth;
+      sliderMain.style = `transform: translateX(${postionX}px)`;
+      index--;
+    }
+    [...dotItems].forEach((el) => el.classList.remove("active"));
+    dotItems[index].classList.add("active");
+  }
+});
